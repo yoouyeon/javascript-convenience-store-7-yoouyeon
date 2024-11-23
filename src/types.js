a6a064ae66a2f093c15d91a08d5ebd18104ddf23
yoouyeon <jyeon.yoon59@gmail.com>
2024-11-23 15:29:54 +0900

    style(jsdoc): 사용하지 않는 타입 정의 제거

--- src/types.js
+++ src/types.js
@@ -18,12 +18,6 @@
  */
 
 /**
- * @typedef {Object} RequestStockType - 재고 관련 요청 타입
- * @property {number} normal - 일반 상품 수량
- * @property {number} promotion - 프로모션 상품 수량
- */
-
-/**
  * @typedef {Object} SinglePromotionRawDataType - csv 파일에서 읽어온 프로모션 정보
  * @property {string} name - 프로모션명
  * @property {string} buy - 구매 수량
@@ -37,25 +31,11 @@
  */
 
 /**
- * @typedef {Map<string, PromotionInfoType>} PromotionMapType - 프로모션명을 key로 가지는 전체 프로모션 정보
- */
-
-/**
  * @typedef {Object} PromotionQuantityType - 프로모션 적용 수량 정보
  * @property {number} promo - 프로모션이 적용되는 정가 구매 수량
  * @property {number} free - 프로모션이 적용되는 증정 수량
  * @property {number} nonPromo - 프로모션이 적용되지 않는 수량
  * @property {number} total - 전체 구매 수량
- */
-
-/**
- * @typedef {Object} RuleType - 검증 규칙
- * @property {Function} isInvalid - 검증 함수
- * @property {string} errorMessage - 에러 메시지
- */
-
-/**
- * @typedef {Record<string, RuleType>} RuleSetType - 검증 규칙 세트
  */
 
 export {};
